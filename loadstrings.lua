2e539b70e4a7655cc9f2e3daa9a8996c248fc8b9
Windows7King <hugo.quintus12@gmail.com>
2024-06-06 23:53:39 +0200

    @loadstrings.lua

---
+++ loadstrings.lua
@@ -0,0 +1,7 @@
+--Loadstrings:
+
+--BTOOLS V3 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Windows7King/rouxhaver_btools/main/BTOOLS_V3.lua"))()
+
+--BTOOLS V5
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Windows7King/rouxhaver_btools/main/BTOOLS_V5.lua"))()
