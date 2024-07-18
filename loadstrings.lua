f5c01b278dd59284300791f24794e2cfd6f18a11
Windows7King <hugo.quintus12@gmail.com>
2024-07-18 21:37:52 +0200

    Update loadstrings.lua

--- loadstrings.lua
+++ loadstrings.lua
@@ -25,3 +25,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Windows7King/scripts/
 
     --Big Arms Stand:
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Windows7King/scripts/main/BIG_ARMS_STAND.lua"))()
+
+--Immortality Lord:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Windows7King/scripts/main/Immortality_Lord.lua"))()
