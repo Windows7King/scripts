8f9610176501b6b1e6c9626f0b4bfd179e5b09d0
Windows7King <hugo.quintus12@gmail.com>
2025-02-12 13:33:04 +0100

    Create Jerk_Off.lua

---
+++ Jerk_Off.lua
@@ -0,0 +1,10 @@
+--Jerk Off Script V2
+--R6 and R15!
+
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+
+if humanoid.RigType == Enum.HumanoidRigType.R15 then
+       loadstring(game:HttpGet("https://pastefy.app/YZoglOyJ/raw"))()
+else
+       loadstring(game:HttpGet("https://pastefy.app/wa3v2Vgm/raw"))() 
+end
